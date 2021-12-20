250e07981a6f272744ad5560b1cf8d94c05ca3cb
Anay Wadhera <anay1018@gmail.com>
2021-12-20 15:25:24 +0900

    gs101: force neuralnetwork HAL to link against libOpenCL
    Change-Id: Ic2652d361dda227a3ed74c42c3796b4024f1dd0c
    Reviewed-on: https://review.statixos.com/c/android_device_google_gs101/+/6456
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>
    Reviewed-on: https://review.statixos.com/c/android_device_google_gs101/+/7427

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -357,13 +357,7 @@ WIFI_AVOID_IFACE_RESET_MAC_CHANGE := true
 WIFI_FEATURE_HOSTAPD_11AX := true
 
 # NeuralNetworks
-GPU_SOURCE_PRESENT := $(wildcard vendor/arm/mali/valhall)
-GPU_PREBUILD_PRESENT := $(wildcard vendor/google_devices/gs101/prebuilts/gpu/libs)
-ifneq "$(or $(GPU_SOURCE_PRESENT),$(GPU_PREBUILD_PRESENT))" ""
 ARMNN_COMPUTE_CL_ENABLE := 1
-else
-ARMNN_COMPUTE_CL_ENABLE := 0
-endif
 ARMNN_COMPUTE_NEON_ENABLE := 1
 
 # Boot.img
