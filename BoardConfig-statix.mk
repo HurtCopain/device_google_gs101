f8b0f4f838dd7dea2a6c0b2a3628486a8453a568
Anay Wadhera <anay1018@gmail.com>
2022-04-07 06:03:09 +0900

    gs101: force disable kernel build
    Change-Id: I9d22eda84a6dc86969e8a3120737eda98e654c64
    Reviewed-on: https://review.statixos.com/c/android_device_google_gs101/+/7496
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>

--- BoardConfig-statix.mk
+++ BoardConfig-statix.mk
@@ -17,6 +17,9 @@ AB_OTA_PARTITIONS += \
 # AVB flags
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
+# Kernel
+TARGET_NO_KERNEL_OVERRIDE := true
+
 # Manifests
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs101/lineage_compatibility_matrix.xml
 
